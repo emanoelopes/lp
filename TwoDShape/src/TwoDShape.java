97e23efe4f9ec698af3c7f38587b851d48e070dc
Emanoel <emanoel@emanoel.pro.br>
2014-06-14 21:08:03 -0300

    Por que sobrepor métodos?

--- TwoDShape/src/TwoDShape.java
+++ TwoDShape/src/TwoDShape.java
@@ -5,27 +5,32 @@
 class TwoDShape {
 	private double width;
 	private double height;
+	private String name;
 	
 	//Construtor padrão
 	TwoDShape(){
 		width = height = 0.0;
+		name = "none";
 	}
 	
 	//Construtor parametrizado
-	TwoDShape(double w, double h){
+	TwoDShape(double w, double h, String n){
 		height = h;
 		width = w;
+		name = n;
 	}
 	
 	//Constrói objeto com altura e largura iguais.
-	TwoDShape(double x){
+	TwoDShape(double x, String n){
 		width = height = x;
+		name = n;
 	}
 	
 	//Constrói um objeto a partir de outro.
 	TwoDShape(TwoDShape ob){
 		width = ob.width;
 		height = ob.height;
+		name = ob.name;
 	}
 	
 	//Métodos acessadores para width e height
@@ -34,8 +39,15 @@ class TwoDShape {
 	double getHeight() {return height;}
 	void setHeight(double h) {this.height = h;}
 	
+	String getName(){return name;}
+	
 	void showDim(){
 		System.out.println("Width and hight are " +width + " and " + height);
+	}
+	
+	double area(){
+		System.out.println("area() must be overridden");
+		return 0.0;
 	}
 	
 }
@@ -52,14 +64,14 @@ class Triangle extends TwoDShape{
 	
 	//Construtor parametrizado.
 	Triangle(String s, double w, double h){
-		super(w, h);//Chama o construtor parametrizado da superclasse.
+		super(w, h, "triangle");//Chama o construtor parametrizado da superclasse.
 
 		style = s;
 	}
 
 	//Construtor com um argumento.
 	Triangle(double x){
-		super(x); //Chama o construtor da superclasse com um argumento.
+		super(x, "triangle"); //Chama o construtor da superclasse com um argumento.
 		
 		style = "filled";
 	}
@@ -69,6 +81,7 @@ class Triangle extends TwoDShape{
 		super(ob);//Passa o objeto para o construtor de TwoDShape
 		style = ob.style;
 	}
+	//Sobrepõe area() para Triangle
 	double area(){
 		return getWidth() * getHeight() / 2;
 	}
@@ -78,43 +91,50 @@ class Triangle extends TwoDShape{
 	}
 }
 
-//Estende Triangle
-class ColorTriangle extends Triangle{
-	private String color;
-	
-	ColorTriangle(String c, String s, double w, double h){
-		super(s, w, h);
-	
-		color = c;
+//Subclasse de TwoDShape para retângulos
+class Rectangle extends TwoDShape{
+	//Construtor padrão
+	Rectangle() {
+		super();
+	}
+	//Construtor para Rectangle
+	Rectangle(double w, double h){
+		super(w, h, "rectangle");//chama o construtor da superclasse
+	}
+	//Constrói um quadrado
+	Rectangle(double x){
+		super(x, "rectangle");//Chama o construtor da superclasse.
 	}
 	
-	String getColor() {return color;}
-	
-	void showColor() {
-		System.out.println("Color is "+color);
+	//Constrói um objeto a partir de outro
+	Rectangle(Rectangle ob){
+		super(ob);//passa o objeto para o construtor de TwoDShape.
 	}
+	
+	boolean isSquare(){
+		if(getWidth() == getHeight()) return true;
+		return false;
+	}
+	//Sobrepõe area() para Rectangle.
+	double area(){
+		return getWidth() * getHeight();
 }
 
-class Shapes7{
-	public static void main(String[] args) {
-		Triangle t1 = new Triangle("outlined", 8.0, 12.0);
-	
-		//faz uma cópia de t1
-		Triangle t2 = new Triangle(t1);
+class DynShapes{
+	public static void main(String args[]) {
+		TwoDShape shapes[] = new TwoDShape[5];
+		shapes[0] = new Triangle("outlined", 8.0, 12.0);
+		shapes[1] = new Rectangle(10);
+		shapes[2] = new Rectangle(10, 4);
+		shapes[3] = new Triangle(7.0);
+		shapes[4] = new TwoDShape(10, 20, "generic");
 		
-		System.out.println("Info for t1: ");
-		t1.showStyle();
-		t1.showDim();
-		System.out.println("Area is "+ t1.area());
-		
-		System.out.println();
-		
-		System.out.println("Info for t2: ");
-		t2.showStyle();
-		t2.showDim();
-		System.out.println("Area is "+ t2.area());
-		
-		
+		for (int i = 0; i < shapes.length; i++) {
+			System.out.println("Object is "+shapes[i].getName());
+			System.out.println("Area is "+shapes[i].area());
+			System.out.println();
+		}
+	}
 		
 	}
 }
